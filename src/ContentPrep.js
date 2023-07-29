1959f16ef7799fbcc7e08ac1cc45c7e514a70031
Luke Ferris <luke.ferris@gmail.com>
2023-07-29 18:27:46 +0300

    Added guidance on image design

--- src/ContentPrep.js
+++ src/ContentPrep.js
@@ -1,6 +1,7 @@
 // For each component, insert a comment directly above the usage of the component explaining its purpose in the page.
 // For each parameter of the component a comment should also be inserted directly above the usage of the component. The comment should clearly specify any constraints or expected ranges of values (e.g. length of the text or dimensions of the image).
 // The constraints for parameters are shown in the definitions.
+// Look to the overall specification for hints on image types (e.g. photo vs cartoon or other styles) and include these in the image alt tag comments to guide image creation.
 // This should be done for every usage of the component - so if Headline is used multiple times, the same comments should be inserted above each usage of the Headline component.
 // Including a comment per parameter above each component in the code is vital. Without this, the downstream content generating code will not function.
 // Make sure that you don't accidentally add a javascript comment '//' where it should be a comment like this: {/* */}
