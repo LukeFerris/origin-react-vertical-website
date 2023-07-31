2da637c1c34443142e7d98bfc6f7a81cf0675876
Luke Ferris <luke.ferris@gmail.com>
2023-07-31 09:33:05 +0200

    Added minor update to the content prep

--- src/ContentPrep.js
+++ src/ContentPrep.js
@@ -1,5 +1,7 @@
+// Only add comments for things that will be rendered (e.g. HTML elements and React components)
+// Do not add comments for anything else (e.g. do not add comments for import statements)
 // For each component, insert a comment directly above the usage of the component explaining its purpose in the page.
-// For each parameter of the component a comment should also be inserted directly above the usage of the component. The comment should clearly specify any constraints or expected ranges of values (e.g. length of the text or dimensions of the image).
+// For each parameter of the component a comment should also be inserted inline next to the parameter. The comment should clearly specify any constraints or expected ranges of values (e.g. length of the text or dimensions of the image).
 // The constraints for parameters are shown in the definitions.
 // Look to the overall specification for hints on image types (e.g. photo vs cartoon or other styles) and include these in the image alt tag comments to guide image creation.
 // This should be done for every usage of the component - so if Headline is used multiple times, the same comments should be inserted above each usage of the Headline component.
