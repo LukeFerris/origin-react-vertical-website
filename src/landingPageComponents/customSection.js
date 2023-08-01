fc50865dfffd9f26e5f8ed074dc45243e8b0dba1
Luke Ferris <luke.ferris@gmail.com>
2023-08-01 14:59:29 +0200

    Updated assertions

--- src/landingPageComponents/customSection.js
+++ src/landingPageComponents/customSection.js
@@ -1,13 +1,14 @@
 // ASSERTIONS:
 // The component name is the name mentioned in the specification.
 // The component is self contained with no props required.
-// The styling of the original template has not been changed.
+// The styling of the original template has not been changed
+// Content has only been added in the marked areas for javascript and the div marked for new UI content.
 // Text strings have been replaced with a placeholder of the following format: {text.placeHolderName.content} where placeholderName is a relevant name.
 // Text placeholders are only one level deep. {text.placeholderName.content} is correct, but {text.objectname.placeholderName.content} is not correct.
 // There is a typography variant h3 at the top of the UI content you created that acts as a title
 // Standard text content uses typography variant h2
 // CardMedia components do not have a fixed height - otherwise they get set too small
-// Elements like TextFields are attempting to fill as much horizontal width as possible
+// Elements like TextFields are attempting to fill as much horizontal width as possible within their containing elements.
 // Any images required are imported using the CreateReactApp standard method of import as shown below.
 // END ASSERTIONS
 
@@ -25,7 +26,8 @@ import horseStableImage from "./assets/images/horseStableImage.png";
 // They can then be used as sources like this: {horseStableImage}.
 
 export default function CustomSectionName() {
-  // you may only add additional javascript here.
+  // you may add javascript here
+  // end javascript zone
 
   return (
     // the styles in this div should not be udpated
